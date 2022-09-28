0f0534ddba363c242aafc3eabeab0ca98c4abccb
Will DePue <will@depue.net>
2022-09-28 10:42:04 -0400

    working well enough

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["se-images.campuslabs.com"],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
