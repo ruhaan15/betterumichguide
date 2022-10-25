271547517cafe243a29f16315c12742443e05d03
Ruhaan Doshi <ruhaand@umich.edu>
2022-10-25 15:26:27 -0400

    link logo in navbar to /clubs

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -2,10 +2,12 @@ const NavBar = () => {
     return (
         <>
             <div className="flex items-center justify-between border-b-2 border-[#f5f5f5] bg-white px-6 py-3 ">
-                <div className="flex items-center gap-2">
-                    <h1 className="text-[25px] font-bold text-[#FFCB03]">M</h1>
-                    <a href="/clubs"><h1 className="text-[22px] font-medium">UMich.guide</h1></a>
-                </div>
+                <a href="/clubs">
+                    <div className="flex items-center gap-2">
+                        <h1 className="text-[25px] font-bold text-[#FFCB03]">M</h1>
+                        <h1 className="text-[22px] font-medium">UMich.guide</h1>
+                    </div>
+                </a>
                 <div className="flex h-10 items-center justify-between gap-8 text-[#323232]">
                     <a href="/about"><h3 className="text-md cursor-pointer">About us</h3></a>
                     <h3 className="text-md cursor-pointer">Write a club review</h3>
