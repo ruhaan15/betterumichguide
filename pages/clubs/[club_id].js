1d519bb2447885513c66fcb3cd85db0d435f10f6
Matthew Wade <mattwade@umich.edu>
2022-10-26 01:59:37 -0400

    Edited club specific page

--- pages/clubs/[club_id].js
+++ pages/clubs/[club_id].js
@@ -74,7 +74,7 @@ const ClubInfo = ({ club }) => {
 
     return (
             <>
-                <div className="flex">
+                <div className="flex mb-6">
                     <Image
                         src={club.profilePicture ? `https://se-images.campuslabs.com/clink/images/${club.profilePicture}` : Solar}
                         alt="Club Logo"
@@ -83,11 +83,39 @@ const ClubInfo = ({ club }) => {
                         className="rounded"
                     />
                     <h1 className="text-[22px] font-medium pl-7 py-2 ">{club.name}</h1>
+                    <div className="ml-auto">
+                        <div className="mt-6 ">
+                            
+                            <a href={`mailto:${club.email}`} target="_blank">
+                                {/* <img src="images/cancel.png" width="15">< */}
+                                <button className="align-middle mx-2 bg-white-500 hover:bg-gray-200 text-white font-bold py-2 px-4 rounded btn btn-white" > 
+                                <img src="http://www.clker.com/cliparts/8/3/1/b/13652250221684996142Email%20Icon.svg.med.png" alt="Email"
+                                    width = {40}
+                                    height = {40}
+                                ></img></button>
+                            </a>
+                            {/* <button onclick="href">Email</button> */}
+                                <a href={club.socialMedia_instagramUrl} target="_blank">
+                                    <button className="align-middle mx-2 bg-white-500 hover:bg-gray-200 text-white font-bold py-2 px-4 rounded btn btn-white">
+                                    {/* <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg> */}
+                                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/124px-Instagram_logo_2016.svg.png" alt="Club Logo"
+                                        
+                                            width={30}
+                                            height={30}
+                                            className="rounded"
+                                        ></img>
+                                    </button>
+                                </a>
+                            <a href={club.socialMedia_externalWebsite} target="_blank">
+                                <button className="align-middle mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded btn btn-blue">Website </button>
+                            </a>
+                        </div>
+                    </div>
                 </div>
 
-                {
-                    parse(club.description)
-                }
+                <div>
+                    {parse(club.description)}
+                </div>
                 {/*<div className="mt-3">{club.description.replace(/<\/?[^>]+(>|$)/g, "")}</div>*/}
 
             </>
