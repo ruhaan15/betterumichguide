36a458bbe40633089b99637350baf82179e21517
Ruhaan Doshi <ruhaandoshi11@gmail.com>
2022-10-27 17:06:04 -0400

    link to TOS and Privacy policy

--- Components/Footer.js
+++ Components/Footer.js
@@ -3,11 +3,11 @@ const Footer = () => {
         <div className="fixed bottom-0 z-20 flex h-10 w-full items-center justify-between bg-[#F3F6FC] px-5 text-sm text-[#38526A]">
             <p>
                 A student project. Not affiliated with the University of Michigan.{" "}
-                <a href="" className="underline">
-                    Read more.
+                <a href="/privacyPolicy" className="underline">
+                    Privacy notice.
                 </a>
             </p>
-            <a href="">
+            <a href="/termsOfUse">
                 <p className="underline">Legal + Deletion Requests</p>
             </a>
         </div>
